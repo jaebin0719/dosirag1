91d10e5ce69f7153d885fcfaead03428b0d6c31e
jaebin0719 <flxksldk@gmail.com>
2023-07-19 16:11:18 +0900

    도시락통 완성

--- js/script.js
+++ js/script.js
@@ -57,6 +57,12 @@ window.onload = function () {
       mbt.classList.add("active");
     }
   });
+  // 모달창 닫기
+  const bodyModal = document.querySelector(".modal");
+  const modalBtn = document.querySelector(".modal-btn");
+  modalBtn.addEventListener("click", function () {
+    bodyModal.style.display = "none";
+  });
   // 반응형 처리
   let winW = window.innerWidth;
   window.addEventListener("resize", function () {
@@ -157,5 +163,35 @@ window.onload = function () {
   const businessModal = document.querySelector(".business-modal");
   businessModal.addEventListener("click", function () {
     businessModal.style.display = "none";
+  });
+  // 위로가기 스크롤바 구현
+  const gotop = document.querySelector(".gotop");
+  gotop.addEventListener("click", function () {
+    window.scrollTo({ top: 0, behavior: "smooth" });
+  });
+  // footer의 상단위치 픽셀값 파악.
+  let footer = document.querySelector(".footer");
+  let footerY = footer.offsetTop;
+  let waypoint_footer = new Waypoint({
+    element: document.querySelector(".footer"),
+    handler: function (direction) {
+      if (direction === "down") {
+        gotop.classList.add("active-footer");
+      } else {
+        gotop.classList.remove("active-footer");
+      }
+    },
+    offset: "95%",
+  });
+  let waypoint_service = new Waypoint({
+    element: document.querySelector(".service"),
+    handler: function (direction) {
+      if (direction === "down") {
+        gotop.classList.add("active");
+      } else {
+        gotop.classList.remove("active");
+      }
+    },
+    offset: "80%",
   });
 };
